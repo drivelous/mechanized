bf32f9e0fc13efb8a5d114a76ac329b4225774de
Sean Dominguez <sean@drivelous.com>
2015-01-16 17:42:37 -0800

    Adds file extensions to download

--- mechanize.py
+++ mechanize.py
@@ -1,43 +1,43 @@
-import cStringIO
-import formatter
-from htmllib import HTMLParser
-import httplib
 import os
 import sys
 import urllib
+import time
 from collections import deque
 from urlparse import urlparse, urljoin
 
 from BeautifulSoup import BeautifulSoup as bs
 
-# TODO:
-# Parse:
-# -Parse all a tags, return all a tags to crawler, and check if each is in a set
-# -Parse all images on a soupified page, check if those filenames have been downloaded
-# -Visit next page
-
 def get_base_url(url):
+	"""Takes any URL and gets its base URL. Used to join
+	paths that lack absolute paths"""
+
 	pieces = urlparse(url)
-	return pieces.scheme + '://' + pieces.netloc
+	return pieces.scheme + "://" + pieces.netloc
 
 def get_filename(img_url):
 	"""Takes image URL, parses, and eliminates excess filepath"""
+
 	parsed = urlparse(img_url)
-	return parsed.path.rstrip('/').split('/')[-1]
+	return parsed.path.rstrip("/").split("/")[-1]
 
 def download(dir_link, filename):
 	"""Downloads image from dir_link giving it name filename"""
+
 	f = open(filename, 'wb')
 	f.write(urllib.urlopen(dir_link).read())
 	f.close()
 
 class Parse(object):
+	"""Takes URL's HTML content, turns it into BeautifulSoup
+	object, and then returns all images and links"""
 
 	def __init__(self, url):
 		self.base_url = get_base_url(url)
 		self.soup = self.soupify(url)
 
 	def soupify(self, url):
+		"""Turns an HTML page into a BeautifulSoup object"""
+
 		html_content = urllib.urlopen(url).read()
 		return bs(html_content)
 
@@ -50,6 +50,7 @@ class Parse(object):
 		return self.soup.findAll('img')
 
 class Crawler(object):
+	"""Keeps track of all links and files it comes across"""
 
 	def __init__(self, url):
 		self.base_url = get_base_url(url)
@@ -57,6 +58,8 @@ class Crawler(object):
 		self.visited = set([])
 		self.images = set([])
 		self.sources = ['src', 'data-src']
+		self.ext = set(['.jpg', '.jpeg', '.gif', '.png', '.docx', '.doc',
+						'.pdf', '.bmp'])
 
 		try:
 			self.directory = self.base_url.split('//')[1]
@@ -65,14 +68,22 @@ class Crawler(object):
 			print "Directory %s exists. Setting as directory..." % self.directory
 
 	def add_or_discard_links(self, links):
+		"""Sanitizes, downloads, or discards each link
+		from a BeautifulSoup object"""
+
 		for counter, link in enumerate(links, start=1):
 			print "Link #" + str(counter)
+			link = link.replace(" ", "").lstrip("..").split("#")[0]
 			if not link.startswith('http'):
 				print "Appending base URL to link ", link
-				link = urljoin(self.base_url, link.replace(" ", ""))
+				link = urljoin(self.base_url, link)
 			if get_base_url(link) != self.base_url:
 				print "DISCARDED: ", link
 				print "Not in same domain\n"
+				continue
+			if os.path.splitext(link)[1] in self.ext:
+				print "Link to file. Processing..."
+				self.process_file(link)
 				continue
 			if link.startswith('mailto:'):
 				print "DISCARDED: ", link
@@ -88,15 +99,25 @@ class Crawler(object):
 				continue
 
 			self.q.append(link)
-			print "ADDED: ", link
-			print "New link! Adding to queue...\n"
+			print "New link! ", link
+			print "Adding to queue...\n"
+
+	def process_file(self, dir_link):
+		"""Extracts filename from dir_link, derives filepath,
+		downloads, and then saves link"""
+
+		filename = get_filename(dir_link)
+		filepath = os.path.join(self.directory, filename)
+		download(dir_link, filepath)
+		self.images.add(dir_link)
+		print "Downloaded %s from %s\n" % (filename, dir_link)
 
 	def download_or_discard_images(self, images):
 		"""Downloads list of BeautifulSoup images"""
 		
-		for img_idx, img in enumerate(images):
-			print "Image #" + str(img_idx + 1) + " of " + str(len(images))
-			for src_idx, src in enumerate(self.sources):
+		for img_idx, img in enumerate(images, start=1):
+			print "Image #" + str(img_idx) + " of " + str(len(images))
+			for src_idx, src in enumerate(self.sources, start=1):
 				try:
 					dir_link = img[src]
 					if not dir_link.startswith('http'):
@@ -104,43 +125,47 @@ class Crawler(object):
 					if dir_link in self.images:
 						print "This image has already been downloaded. Skipping...\n"
 						break
-					filename = get_filename(dir_link)
-					filepath = os.path.join(self.directory, filename)
-					download(dir_link, filepath)
-					self.images.add(dir_link)
-					print "Downloaded %s from %s\n" % (filename, dir_link)
-					# Does not attempt next source - goes to next image
-					break
+					else:
+						self.process_file(dir_link)
+						break # Does not attempt next source - goes to next image
+
 				except KeyError as e:
 					print "Does not have '%s' attribute" % src
-					if src_idx + 1 < len(self.sources):
-						print "Attempting '%s' attribute" % self.sources[src_idx + 1]
+					if src_idx < len(self.sources):
+						print "Attempting '%s' attribute" % self.sources[src_idx]
 					else:
 						print "Sources exhausted. Skipping download.\n"
 
 
 	def process(self, url):
+		"""Extracts all content from one URL"""
+
 		page = Parse(url)
 		images = page.return_images()
 		links = page.return_hrefs()
 		self.add_or_discard_links(links)
 		self.download_or_discard_images(images)
-		# print "self.q: ", self.q
-		# self.add_or_discard_links(links)
 
 	def go(self):
+		"""Processes all uncrawled links until empty"""
+		
+		retries = 0
 		while self.q:
-			url = self.q.popleft()
-			print "Currently on %s. Processing...\n\n" % url
-			self.process(url)
-			self.visited.add(url)
+			url = self.q[0]
+			print "Visiting URL: ", url
+			try:
+				self.process(url)
+				self.visited.add(self.q.popleft())
+			except IOError as errMsg:
+				retries += 1
+				if retries == 5:
+					print "Error threshold met. Quitting...\n"
+					return
 
-			# page = Parse(self.q.popleft())
-			# images = page.return_images()
-			# self.download_or_discard_images(images)
-			# links = page.return_hrefs()
-			# self.add_or_discard_links(links)
-			# self.visited.add(page)
+				print "(%d) There's an IOError. Check below." % retries
+				print errMsg
+				print "Let's wait this out a few seconds.\n"
+				time.sleep(5)
 
 		print "Crawling finished! Wrapping up..."
 
@@ -153,8 +178,9 @@ def main():
 		except (KeyboardInterrupt, EOFError):
 			print "You didn't enter a URL to crawl. Aborting..."
 			return
-	if not url.startswith('http://') and \
+	if not url.startswith('http') and \
 		not url.startswith('ftp://'):
+		print "url: ", url
 		url = 'http://%s/' % url
 
 	mechanized = Crawler(url)
